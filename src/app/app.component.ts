e6d918c4645f1a8b5194f165e075192da391e429
KGRozanski <kgrozanski@gmail.com>
2022-04-07 20:04:22 +0200

    cleared google auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,19 +18,13 @@ export class AppComponent {
 
 
       google.accounts.id.initialize({
-        client_id: "995696971353-a56rf0rf3q01sdf44ohefkq0fpf9qut1.apps.googleusercontent.com",
-        callback: this.handleCredentialResponse
+        client_id: "995696971353-a56rf0rf3q01sdf44ohefkq0fpf9qut1.apps.googleusercontent.com"
       });
-      google.accounts.id.renderButton(
-        document.getElementById("buttonDiv"),
-        { theme: "outline", size: "large" }  // customization attributes
-      );
+
       google.accounts.id.prompt(); // also display the One Tap dialog
     
   }
 
 
-  public handleCredentialResponse(response: any) {
-    console.log("Encoded JWT ID token: " + response.credential);
-  }
+
 }
