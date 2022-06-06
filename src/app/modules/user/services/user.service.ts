92a1e6e2966b9fb0d91bb31b26a3f62c967a0c77
KGRozanski <kgrozanski@gmail.com>
2022-06-06 10:25:45 +0200

    feat(): withCredentials cookies set properly

--- src/app/modules/user/services/user.service.ts
+++ src/app/modules/user/services/user.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { RegisterUser } from '../interfaces/register-user.interface';
@@ -18,13 +18,14 @@ export class UserService {
 
   public login(credentials: UserCredentials): Observable<Object> {
 
+
     const BODY = {
       ...credentials,
       grant_type: 'password',
       client_id: '2ca963453d271c5398e52d2510109217'
     }
 
-    return this.http.post('http://localhost:3000/api/auth/local', BODY);
+    return this.http.post('http://localhost:3000/api/auth/local', BODY, {withCredentials: true});
   }
 }
 //Zaq1@wsx1234
