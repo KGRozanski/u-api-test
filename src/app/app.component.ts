de053c8533ce99a28b6a2f926baa1f0e9556dc73
KGRozanski <kgrozanski@gmail.com>
2022-07-05 13:00:52 +0200

    fix(): Google one-tap moved to register-form cmp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component, Inject, OnInit, Renderer2 } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { SettingsSelectors } from './core/selectors/settings.selectors';
 
-declare const google: any
+
 
 @Component({
   selector: 'app-root',
@@ -15,7 +15,6 @@ export class AppComponent implements OnInit {
 
   title: string = 'u-api-test';
 
-
   constructor(
     private store: Store,
     @Inject(DOCUMENT) private document: Document,
@@ -23,17 +22,9 @@ export class AppComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-
-      google.accounts.id.initialize({
-        client_id: "995696971353-a56rf0rf3q01sdf44ohefkq0fpf9qut1.apps.googleusercontent.com"
-      });
-
-      // google.accounts.id.prompt(); // also display the One Tap dialog
-
-      this.store.select(SettingsSelectors.selectSettingsCollection).subscribe((data) => {
-        this.renderer.setAttribute(this.document.body, 'class', data.isDarkModeEnabled ? 'darkMode' : '');
-      });
-
+    this.store.select(SettingsSelectors.selectSettingsCollection).subscribe((data) => {
+      this.renderer.setAttribute(this.document.body, 'class', data.isDarkModeEnabled ? 'darkMode' : '');
+    });
   }
 
 
--- src/app/modules/user/components/register-form/register-form.component.ts
+++ src/app/modules/user/components/register-form/register-form.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CustomValidators } from '../../../../core/validators/validators';
 import { UserService } from '../../services/user.service';
+
+declare let google: any;
+
 @Component({
   selector: 'app-register-form',
   templateUrl: './register-form.component.html',
   styleUrls: ['./register-form.component.scss']
 })
-export class RegisterFormComponent implements OnInit {
+export class RegisterFormComponent {
 
-  constructor(private FB: FormBuilder, private US: UserService) { }
+  private googleScript: HTMLScriptElement = this.document.createElement("script");
+
+  constructor(private FB: FormBuilder, private US: UserService, @Inject(DOCUMENT) private document: Document,) {
+    this.googleScript.src = "https://accounts.google.com/gsi/client";
+    this.document.body.appendChild(this.googleScript);
+  }
   
   public registerForm: FormGroup = this.FB.group({
     username: [null, 
@@ -46,17 +55,21 @@ export class RegisterFormComponent implements OnInit {
   },  {
     validators: CustomValidators.passwordEquality
   });
-  
-  
-  ngOnInit(): void {}
 
+  ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+    this.googleScript.onload = () => {
+      google.accounts.id.initialize({
+        client_id: "995696971353-a56rf0rf3q01sdf44ohefkq0fpf9qut1.apps.googleusercontent.com"
+      });
+    };
+  }
 
   submit() {
     this.US.register(this.registerForm.value).subscribe((res) => {
       console.log(res)
     })
   }
-
-
 
 }
