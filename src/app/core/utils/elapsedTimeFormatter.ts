67b3ad1c6402bcd352b4957052cdd41d33114b50
KGRozanski <kgrozanski@gmail.com>
2022-07-09 18:28:47 +0200

    feat(): elapsed time formatter

---
+++ src/app/core/utils/elapsedTimeFormatter.ts
@@ -0,0 +1,62 @@
+/**
+ * 
+ * @param n Time left time in milliseconds
+ * @returns 
+ */
+
+export function elapsedTimeFormatter(n: number) {
+    let time = n;
+
+    const days = Math.floor(time / 86400000);
+    time = time - days * 86400000;
+
+    const hours = Math.floor(time / 3600000);
+    time = time - hours * 3600000;
+
+    const minutes = Math.floor(time / 60000);
+    time = time - minutes * 60000;
+
+    const seconds = Math.floor(time / 1000);
+    time = time - seconds * 1000;
+
+    let output = '';
+
+    if (days > 0) {
+        if (days >= 10) {
+            output += days + ' days ';
+        } else {
+            output += '0' + days + ' days ';
+        }
+    }
+    if (hours > 0) {
+        if(hours >= 10) {
+            output += hours + ':';
+        } else {
+            output += '0' + hours + ':';
+        }
+    } else {
+        output += '00:';
+    }
+
+    if(minutes > 0) {
+        if (minutes >= 10) {
+            output += minutes + ':';
+        } else {
+            output += '0' + minutes + ':';
+        }
+    }
+
+    if(seconds > 0) {
+        if(seconds >= 10) {
+            output += seconds;
+        } else {
+            output += '0' + seconds;
+        }
+    }
+
+    if(time > 0) {
+        output += '.'+time;
+    }
+
+    return output;
+}
