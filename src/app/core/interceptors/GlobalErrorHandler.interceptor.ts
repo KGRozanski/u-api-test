2444a742baebafb11404bbe9a81226097da32fb1
KGRozanski <kgrozanski@gmail.com>
2022-07-09 18:28:04 +0200

    fix(): removed exception for /token request in global interceptor

--- src/app/core/interceptors/GlobalErrorHandler.interceptor.ts
+++ src/app/core/interceptors/GlobalErrorHandler.interceptor.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpEvent, HttpHandler, HttpRequest, HttpErrorResponse } from '@angular/common/http';
-import { catchError, Observable, throwError } from 'rxjs';
+import { catchError, Observable, of, throwError } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { NotificationActions } from '../actions/notifications.actions';
 import { NotificationType } from '../enums/notification-type.enum';
@@ -10,16 +10,17 @@ export class GlobalErrorHandlerInterceptor implements HttpInterceptor {
 
     constructor(private store: Store) {}
 
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<any> {
         return next.handle(req).pipe(
             catchError((error) => {
 
+                
                 if (error instanceof HttpErrorResponse) {
-                    console.log('http error occured', error);
+                    console.log('[GLOBAL INTERCEPTOR] http error occured', error);
                     this.store.dispatch(NotificationActions.push({notification: {type: NotificationType.ERROR, message: error.error.message}}));
                 }
 
-                return throwError(() => new Error(error));
+                throw(error);
             })
         )
     }
