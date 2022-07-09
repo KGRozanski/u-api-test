53cc002a7564096b0f907cdcccf0ea89f3d06ac6
KGRozanski <kgrozanski@gmail.com>
2022-07-09 18:30:41 +0200

    feat(): Methods related to cookies moved to cookie service

--- src/app/modules/shared/services/cookie.service.ts
+++ src/app/modules/shared/services/cookie.service.ts
@@ -1,18 +1,27 @@
-import { Injectable } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Inject, Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CookieService {
 
-  constructor() { }
+  constructor(@Inject(DOCUMENT) private document: Document) { }
 
 
   public getCookie(name: string) {
-    const value = `; ${document.cookie}`;
+    const value = `; ${this.document.cookie}`;
     const parts = value.split(`; ${name}=`);
     if (parts.length === 2) return parts.pop()?.split(';').shift();
     else return undefined;
+  }
+
+  public setCookie(name: string, value: string, exp: string) {
+    this.document.cookie = name + '=' + value + '; expires=' + exp+ '; path=/;';
+  }
+
+  public doesCookieExist(name: string): boolean {
+    return !!this.getCookie(name);
   }
 
 }
