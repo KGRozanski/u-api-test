19ebc9ba3b935ba108857c345f987caec7c17dbf
KGRozanski <kgrozanski@gmail.com>
2022-08-10 16:01:24 +0200

    feat(): fully integrated registration

--- src/app/modules/user/components/register-form/register-form.component.ts
+++ src/app/modules/user/components/register-form/register-form.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { NotificationActions } from 'src/app/core/actions/notifications.actions';
+import { NotificationType } from 'src/app/core/enums/notification-type.enum';
 import { CustomValidators } from '../../../../core/validators/validators';
 import { UserService } from '../../services/user.service';
 
@@ -10,7 +13,7 @@ import { UserService } from '../../services/user.service';
 })
 export class RegisterFormComponent {
 
-  constructor(private FB: FormBuilder, private US: UserService) {}
+  constructor(private FB: FormBuilder, private US: UserService, private store: Store) {}
   
   public registerForm: FormGroup = this.FB.group({
     username: [null, 
@@ -51,9 +54,9 @@ export class RegisterFormComponent {
 
 
   submit() {
-    this.US.register(this.registerForm.value).subscribe((res) => {
-      console.log(res)
-    })
+    this.US.register(this.registerForm.value).subscribe((res: any) => {
+      this.store.dispatch(NotificationActions.push({notification: {type: NotificationType.SUCCESS, message: res['message']}}));
+    });
   }
 
 }
--- src/app/modules/user/services/user.service.ts
+++ src/app/modules/user/services/user.service.ts
@@ -3,29 +3,19 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { RegisterUser } from '../interfaces/register-user.interface';
 import { UserCredentials } from '../interfaces/user-credentials.interface';
+import { ApiLinksService } from './api-links.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private readonly http: HttpClient) { }
+  constructor(private readonly http: HttpClient, private apiLinks: ApiLinksService,) { }
 
 
   public register(user: RegisterUser): Observable<Object> {
-    return this.http.post('http://localhost:3000/api/accounts', user);
+    return this.http.post(this.apiLinks.apiLink + 'accounts', user);
   }
 
-  public login(credentials: UserCredentials): Observable<Object> {
-
-
-    const BODY = {
-      ...credentials,
-      grant_type: 'password',
-      client_id: '2ca963453d271c5398e52d2510109217'
-    }
-
-    return this.http.post('http://localhost:3000/api/auth/local', BODY, {withCredentials: true});
-  }
 }
 //Zaq1@wsx1234
