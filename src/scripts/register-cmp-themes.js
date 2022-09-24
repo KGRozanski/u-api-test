c883e2954d7fac4fa6858a2e015c3be89624b6f1
KGRozanski <kgrozanski@gmail.com>
2022-09-24 16:11:35 +0200

    feat(REGISTER-CMP-THEMES): Path resolve

--- src/scripts/register-cmp-themes.js
+++ src/scripts/register-cmp-themes.js
@@ -2,17 +2,21 @@ const fs = require('fs');
 const path = require('path');
 
 (function() {
-    let resolved = path.resolve(process.cwd(), './src/assets/scss/themes')
+    const PATH = path.resolve(process.cwd(), './src/assets/scss/themes')
 
-    console.log('platform: ' + process.platform);
-    console.log(resolved)
-    fs.readdir(resolved, (err, files) => {
+
+    console.log(PATH)
+    fs.readdir(PATH, (err, files) => {
         if(err) { throw err; }
 
-        const switchFileIdx = files.findIndex('__theme-switch.scss');
+        const switchFileIdx = files.findIndex((el) => el === '__theme-switch.scss');
+
 
         if(switchFileIdx > -1) {
-
+            files.splice(switchFileIdx, 1);
+            fs.rm(path.resolve(PATH + '/__theme-switch.scss'), (err) => { if (err) {throw err} });
         }
+
+        const parsed
     });
 })();
