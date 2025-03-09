d6e024bd02b4529601b05f92fbff6c09d8588395
Kamil <kgrozanski@gmail.com>
2025-03-09 18:35:03 +0100

    v18

--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RegisterFormComponent } from './components/register-form/register-form.component';
 import { SharedModule } from '../shared/shared.module';
 import { LoginFormComponent } from './components/login-form/login-form.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { GoogleAuthComponent } from './components/google-auth/google-auth.component';
 import { TableOfUsersForAdminsComponent } from './components/table-of-users-for-admins/table-of-users-for-admins.component';
 import { AvatarComponent } from './components/avatar/avatar.component';
@@ -12,24 +12,21 @@ import { RouterModule } from '@angular/router';
 import { AccountInfoComponent } from './components/account-info/account-info.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
-@NgModule({
-	declarations: [
-		RegisterFormComponent,
-		LoginFormComponent,
-		GoogleAuthComponent,
-		TableOfUsersForAdminsComponent,
-		AvatarComponent,
-		ResetPasswordFormComponent,
-		AccountInfoComponent,
-	],
-	imports: [CommonModule, ReactiveFormsModule, RouterModule, SharedModule, HttpClientModule],
-	exports: [
-		RegisterFormComponent,
-		LoginFormComponent,
-		TableOfUsersForAdminsComponent,
-		AvatarComponent,
-		ResetPasswordFormComponent,
-		AccountInfoComponent,
-	],
-})
+@NgModule({ declarations: [
+        RegisterFormComponent,
+        LoginFormComponent,
+        GoogleAuthComponent,
+        TableOfUsersForAdminsComponent,
+        AvatarComponent,
+        ResetPasswordFormComponent,
+        AccountInfoComponent,
+    ],
+    exports: [
+        RegisterFormComponent,
+        LoginFormComponent,
+        TableOfUsersForAdminsComponent,
+        AvatarComponent,
+        ResetPasswordFormComponent,
+        AccountInfoComponent,
+    ], imports: [CommonModule, ReactiveFormsModule, RouterModule, SharedModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class UserModule {}
