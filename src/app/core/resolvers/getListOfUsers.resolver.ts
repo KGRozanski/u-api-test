56b1b88f19dd0da758bee60744e4ec45ea66d9ad
Kamil <kgrozanski@gmail.com>
2025-03-09 17:12:10 +0100

    v16

--- src/app/core/resolvers/getListOfUsers.resolver.ts
+++ src/app/core/resolvers/getListOfUsers.resolver.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Resolve } from '@angular/router';
+
 import { Observable } from 'rxjs';
 import { UserService } from 'src/app/modules/user/services/user.service';
 import { HttpResponse } from '@angular/common/http';
 import AccountExtended from 'src/app/modules/user/interfaces/account-extended.interface';
 
 @Injectable()
-export class GetListOfUsersResolver implements Resolve<Observable<HttpResponse<[AccountExtended[], number]>>> {
+export class GetListOfUsersResolver  {
 	constructor(private US: UserService) {}
 
 	resolve(): Observable<HttpResponse<[AccountExtended[], number]>> {
